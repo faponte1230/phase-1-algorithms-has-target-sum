cd3ca9e6907ea67a59499d332a2cb0701712d3ad
Fernando Aponte <faponte1230@yahoo.com>
2022-12-08 21:07:00 -0500

    commit tree a4c1c4fa951ca0e2ab7c2089cde06c51e2f84b5b

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+  for (let i = 0; i < array.length; i++) {
+    let difference = target - array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === difference) return true
+    }
+  }
+
+  return false;
+
 }
+
 
 /* 
   Write the Big O time complexity of your function here
